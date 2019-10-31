547c993eee7330e445ace15efa5765e99f66f827
Carlos Dominguez Garcia <carlosdg97@gmail.com>
2019-10-31 19:47:25 +0000

    Added a login endpoint that returns a JWT

--- src/index.js
+++ src/index.js
@@ -10,6 +10,16 @@ app.use(cors())
 // API
 app.get('/api', (req, res) => res.json({ message: "My message" }))
 app.post('/api/posts', (req, res) => res.json({ message: "Post created" }))
+app.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        name: "pepito",
+        email: "pepito@gmail.com"
+    }
+    const secret = 'alumno2019'
+
+    jwt.sign({ user }, secret, (err, token) => res.json({ token }))
+})
 
 // Run server
 app.listen(3000, () => console.log('API running on port 3000'))
