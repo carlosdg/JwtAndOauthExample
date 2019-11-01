8e3fca7496153cf2448b688fa76bad3d4813c82b
Carlos Dominguez Garcia <carlosdg97@gmail.com>
2019-11-01 18:16:27 +0000

    Using Github's API with OAuth

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+const { readFileSync } = require("fs");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const axios = require("axios");
 const verifyToken = require("./verifyToken");
 
 const app = express();
 const JWT_SECRET = "alumno2019";
+const GH_OAUTH_CLIENT_ID = "CLIENT_ID";
+const GH_OAUTH_SECRET_ID = readFileSync(
+  __dirname + "/GithubOauthClientSecret.secret"
+);
 
 // Express middleware
 app.use(cors());
+app.use(express.static(__dirname + "/public"));
 
-// API endpoints
+// API routes
 app.get("/api", (req, res) => res.json({ message: "My message" }));
 
 app.post("/api/login", (req, res) => {
@@ -30,6 +37,19 @@ app.post("/api/posts", verifyToken, (req, res) => {
     } else {
       res.json({ message: "Post created", authData });
     }
+  });
+});
+
+app.get("/oauth/redirect", (req, res) => {
+  const requestToken = req.query.code;
+
+  axios({
+    method: "post",
+    url: `https://github.com/login/oauth/access_token?client_id=${GH_OAUTH_CLIENT_ID}&client_secret=${GH_OAUTH_SECRET_ID}&code=${requestToken}`,
+    headers: { accept: "application/json" }
+  }).then(response => {
+    const accessToken = response.data.access_token;
+    res.redirect(`/welcome.html?access_token=${accessToken}`);
   });
 });
 
